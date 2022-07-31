a0ccdfc7cb28baa514a6e8b5bd51386c56a2e107
zhenwei1108 <zhenwei1108@163.com>
2022-07-31 23:08:51 +0800

    完善client的构造

--- glass-core/src/main/java/com/github/zhenwei/glass/core/client/DefaultNettyClient.java
+++ glass-core/src/main/java/com/github/zhenwei/glass/core/client/DefaultNettyClient.java
@@ -1,26 +1,34 @@
 package com.github.zhenwei.glass.core.client;
 
 import com.github.zhenwei.glass.common.exceptions.GlassNettyException;
+import com.github.zhenwei.glass.core.channel.AbstractNettyChannelInitializer;
 import io.netty.bootstrap.Bootstrap;
-import io.netty.channel.AdaptiveRecvByteBufAllocator;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelOption;
+import io.netty.channel.*;
+import io.netty.channel.socket.SocketChannel;
 import io.netty.handler.logging.LogLevel;
+import io.netty.util.concurrent.GenericFutureListener;
 
 public class DefaultNettyClient extends AbstractNettyClient {
 
     public DefaultNettyClient(String host, int port) throws GlassNettyException {
-        DefaultProviderChannelInitializer initializer = new DefaultProviderChannelInitializer();
-        ChannelFuture future = build(host, port, initializer, null, LogLevel.INFO);
-
-
+        this(host, port, new DefaultProviderChannelInitializer());
     }
+
+    public DefaultNettyClient(String host, int port, AbstractNettyChannelInitializer initializer) throws GlassNettyException {
+        this(host, port, initializer, null, LogLevel.INFO);
+    }
+
+    public DefaultNettyClient(String host, int port, ChannelInitializer<SocketChannel> initializer,
+                              GenericFutureListener<ChannelPromise> listener, LogLevel level) throws GlassNettyException {
+        build(host, port, initializer, listener, level);
+    }
+
 
     @Override
     void operate(Bootstrap bootstrap) {
         bootstrap
                 .option(ChannelOption.SO_KEEPALIVE, true)
-                .option(ChannelOption.SO_TIMEOUT, 2000)
+                .option(ChannelOption.SO_TIMEOUT, 3000)
                 .option(ChannelOption.AUTO_CLOSE, true)
                 //发送缓冲区大小, 内核参数: net.core.wmem_max
                 .option(ChannelOption.SO_SNDBUF, 1024)
