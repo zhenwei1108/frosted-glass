f64ec80b0c67a86273d1b07e1be7de671a632f4f
zhenwei1108 <zhenwei1108@163.com>
2022-08-02 22:59:48 +0800

    updateName

--- glass-core/src/main/java/com/github/zhenwei/glass/core/client/DefaultClientChannelInitializer.java
+++ glass-core/src/main/java/com/github/zhenwei/glass/core/client/DefaultClientChannelInitializer.java
@@ -1,6 +1,7 @@
 package com.github.zhenwei.glass.core.client;
 
 import com.github.zhenwei.glass.core.channel.AbstractNettyChannelInitializer;
+import com.github.zhenwei.glass.core.client.coder.DefaultChannelOutboundHandler;
 import io.netty.channel.ChannelPipeline;
 
 /**
@@ -9,10 +10,11 @@ import io.netty.channel.ChannelPipeline;
  * @author: zhangzhenwei
  * @date: 2021/12/26 21:42
  */
-public class DefaultProviderChannelInitializer extends AbstractNettyChannelInitializer {
+public class DefaultClientChannelInitializer extends AbstractNettyChannelInitializer {
 
     @Override
     public void bindOutboundHandler(ChannelPipeline pipeline) {
         //add list of OutboundHandler
+        pipeline.addLast(new DefaultChannelOutboundHandler());
     }
 }
--- glass-core/src/main/java/com/github/zhenwei/glass/core/client/IoClientHolder.java
+++
@@ -1,8 +0,0 @@
-package com.github.zhenwei.glass.core.client;
-
-import java.util.concurrent.ConcurrentHashMap;
-
-public class IoClientHolder {
-
-    private static final ConcurrentHashMap<String, INioClient> clients = new ConcurrentHashMap<>(1);
-}
